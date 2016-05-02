1f9e1871a488a6aa39d74363e1bf74e32379f34e
Stefan Szarek <stefan.szarek@gmail.com>
2016-05-02 20:08:40 +0200

    Extracted chat communication code to separate class

--- client.js
+++ client.js
@@ -1,57 +1,42 @@
+'use strict';
+
+// const readline = require('readline');
+// const Redis = require('redis-fast-driver');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// const subscribeConnection = new Redis({
+//     host: '127.0.0.1',
+//     port: 6379
+// });
+
+// const publishConnection = new Redis({
+//     host: '127.0.0.1',
+//     port: 6379
+// });
+
 const readline = require('readline');
-const Redis = require('redis-fast-driver');
+const ChatClient = require('./lib/chatClient');
+const CommandRgxp = /^\/(\w+) (\w+)$/g;
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const subscribeConnection = new Redis({
-    host: '127.0.0.1',
-    port: 6379
-});
+const client = new ChatClient('127.0.0.1', 6379);
+client.connect();
 
-const publishConnection = new Redis({
-    host: '127.0.0.1',
-    port: 6379
-});
-
-const CommandRgxp = /^\/(\w+) (\w+)$/g;
-
-let currentRoom = '';
-
-subscribeConnection.on('ready', function () {
-    rl.write('Subscribe connection established\n');
-});
-
-subscribeConnection.on('error', function (e) {
-    rl.write(e);
-});
-
-publishConnection.on('ready', function () {
-    rl.write('Publish connection established\n');
-});
-
-publishConnection.on('error', function (e) {
-    rl.write(e);
-});
+client.on('message', message => console.log(message));
 
 rl.on('line', function (line) {
     let result = CommandRgxp.exec(line);
     if (result) {
         let room = result[2];
-        currentRoom = room;
-        joinRoom(room);
+        client.joinRoom(room);
     } else {
-        sendMessage(line);
+        client.sendMessage(line);
     }
 });
-
-function joinRoom(room) {
-    subscribeConnection.rawCall(['SUBSCRIBE', room], function (e, data) {
-        console.log(data);
-    });
-}
-
-function sendMessage(message) {
-    publishConnection.rawCall(['PUBLISH', currentRoom, message]);
-}
